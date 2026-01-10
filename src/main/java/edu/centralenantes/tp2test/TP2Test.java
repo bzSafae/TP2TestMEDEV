b2202ba75d915cf86dedb1899c04162d397c7423
Bz Safae <bouzidisafa84@gmail.com>
2026-01-10 22:09:05 +0100

    Initialisation

---
+++ src/main/java/edu/centralenantes/tp2test/TP2Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.centralenantes.tp2test;
+
+/**
+ *
+ * @author dodi
+ */
+public class TP2Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
