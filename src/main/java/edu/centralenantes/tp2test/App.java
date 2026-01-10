36122185d910fc26cc0085c67ade1d08c2956baa
Bz Safae <bouzidisafa84@gmail.com>
2026-01-10 22:51:57 +0100

    PGM: read/write + jar + javadoc ready

---
+++ src/main/java/edu/centralenantes/tp2test/App.java
@@ -0,0 +1,20 @@
+package edu.centralenantes.tp2test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dodi
+ */
+import java.nio.file.Path;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        PGMImage img = PGMImage.read(Path.of("brain.pgm"));
+        img.write(Path.of("output.pgm"));
+        System.out.println("OK: saved output.pgm");
+    }
+}
